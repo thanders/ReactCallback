b827ddad761cd3e959af8c0417235e1d10c34132
Thomas Anderson <thomas.anderson@ucdconnect.ie>
2020-02-17 21:03:55 -0700

    Updates

---
+++ precache-manifest.875fefe24df5861ba5abaf20b7817ee4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d06e897f7719feebbd916a7b213a810e",
+    "url": "/thanders/ReactCallback/index.html"
+  },
+  {
+    "revision": "29bdd7755a092cb4b893",
+    "url": "/thanders/ReactCallback/static/css/main.e0d65b33.chunk.css"
+  },
+  {
+    "revision": "d06ba1d53bbbf3b162a6",
+    "url": "/thanders/ReactCallback/static/js/2.81753766.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/thanders/ReactCallback/static/js/2.81753766.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29bdd7755a092cb4b893",
+    "url": "/thanders/ReactCallback/static/js/main.e91c368d.chunk.js"
+  },
+  {
+    "revision": "e6caeebeb9deb6617cf0",
+    "url": "/thanders/ReactCallback/static/js/runtime-main.4b74b229.js"
+  }
+]);
